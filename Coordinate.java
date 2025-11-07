d3aefbf509bc4b5afdcffec3322136833e9f336d
BELLAFKIH.22128 <bellafkih.deyae.studente@itispaleocapa.it>
2025-11-07 12:56:40 +0100

    aggiunta classe coordinate

---
+++ Coordinate.java
@@ -0,0 +1,18 @@
+public class Coordinate {
+
+    private double latitudine;
+    private double longitudine;
+
+    public Coordinate(double latitudine, double longitudine) {
+        this.latitudine = latitudine;
+        this.longitudine = longitudine;
+    }
+
+    public double getLatitudine() {
+        return latitudine = latitudine;
+    }
+
+    public double getLongitudine() {
+        return longitudine;
+    }
+}
